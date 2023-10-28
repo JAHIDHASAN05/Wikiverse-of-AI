737d00bcf25d63ed52dd337d92946f544f7219a3
JAHID HASAN <developerjahid@gmail.com>
2023-10-28 21:59:59 +0600

    modal data detail funtion added

--- js/index.js
+++ js/index.js
@@ -40,15 +40,15 @@ const displayAiData=(datas ,limit)=>{
                                 <h5>${data.name}</h5>
                                  <p>Date:${data.published_in}</p>
                              </div>
-                             <button  class="btn btn-primary " data-bs-toggle="modal" data-bs-target="#phone-detail">Detail</button>
+                             <button onclick="loadAiDetail('${data.id}')"  class="btn btn-primary " data-bs-toggle="modal" data-bs-target="#phone-detail">Detail</button>
 
                              </div>
 
                     </div>
         `
         aiDataContainer.append(aiDiv)
+        // console.log(data)
 
-        console.log(data)
       }
 
       toggleSpinner(false)
@@ -69,6 +69,85 @@ document.getElementById("btn-see-more").addEventListener("click", function(){
         loaderContainer.classList.add("d-none")
     }
  }
+
+
+
+
+//  detail modat data load and display function
+
+const loadAiDetail= async(id)=>{
+    const url=`https://openapi.programming-hero.com/api/ai/tool/${id}`
+    const res = await fetch(url)
+    const data= await res.json()
+    displayAiDetail(data.data)
+    console.log(data)
+    
+}
+
+const displayAiDetail=(data)=>{
+  const ModalDetailBody=document.getElementById("detail-body")
+ModalDetailBody.innerHTML=`
+            <div class="card">
+               
+                <div class="card-body">                        
+                        <h4 class="card-text mb-5">${data.description}</h4>                        
+                        <div class="d-flex justify-content-around mb-5">
+                            <div style="height:100px" class="w-25 bg-light p-3 d-flex justify-content-center align-items-center text-center">
+                                ${data.pricing[0].price} <br> 
+                                ${data.pricing[0].plan}
+                            </div>
+                            <div style="height:100px" class="w-25 bg-light p-3 d-flex justify-content-center align-items-center text-center">
+                                ${data.pricing[1].price} <br> 
+                                ${data.pricing[1].plan}
+                            </div>
+                            <div style="height:100px" class="w-25 bg-light p-3 d-flex justify-content-center align-items-center text-center">
+                                ${data.pricing[2].price} <br> 
+                                ${data.pricing[2].plan}
+                            </div>
+                            
+                        </div>
+                </div>
+                <div class="card-footer">
+                    <div class =" d-flex justify-content-between">
+                        <div>
+                          <h3>Feature                          
+                          </h3>
+                          <ul>
+                            <li>${data.features[1].feature_name}</li>
+                            <li>${data.features[2].feature_name}</li>
+                            <li>${data.features[3].feature_name}</li> 
+                          </ul>
+                        </div>
+                        <div>
+                          <h3>Integrations:                          
+                          </h3>
+                          <ul>
+                            <li>${data.integrations[0]}</li>
+                            <li>${data.integrations[1]}</li>
+                            <li>${data.integrations[2]}</li>                            
+                          </ul>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="${data.image_link[0]}" class="card-img-bottom" alt="No Pictutre Found">
+                <div class="card-body text-center ">
+                     <h5 class="card-title">${data.input_output_examples[0].input}</h5>
+                      <p class="card-text">${data.input_output_examples[0].output}</p>
+                </div>
+                <div class="card-footer">
+                   
+                </div>
+            </div>          
+
+`
+console.log(data.input_output_examples[0])
+
+  console.log(data)
+}
 
 
 LoadAiData()
